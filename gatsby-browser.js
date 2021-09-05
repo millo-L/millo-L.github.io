c05cdb414dcd2e2d746378b33de348a398b426dd
millo-L <seungmin3837@naver.com>
2021-09-05 21:02:38 +0900

    post: react native kakao share link - deep link

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
 require("./static/css/prism-vsc-dark-plus.css");
+export const onClientEntry = async () => {
+    if (typeof IntersectionObserver === "undefined") {
+        await import("intersection-observer");
+    }
+};
